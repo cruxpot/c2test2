1a7427239db6b4979117cb0fdc73445c6c0d5814
Nick Navarro <nnavarro@def-logix.com>
2019-11-21 18:36:29 -0600

    changed public ip to allow all ingress

--- data/scripts/get_public_ip.sh
+++ data/scripts/get_public_ip.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#echo {\"ip\":\""`curl -qs http://ifconfig.me`"\"}
-echo {\"ip\":\""0.0.0.0/0"\"}
+echo {\"ip\":\""`curl -qs http://ifconfig.me`"\"}
--- modules/aws/dns-rdir/security_group.tf
+++ modules/aws/dns-rdir/security_group.tf
@@ -15,7 +15,10 @@ resource "aws_security_group" "dns-rdir" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    /*
     cidr_blocks = ["${data.external.get_public_ip.result["ip"]}/32"]
+    */
   }
   ingress {
     from_port = 53
